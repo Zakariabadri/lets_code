5d46e77ccb41030591b1ebf1018fddd15c0320a4
Zakariabadri <Zak@MacBook-Pro-de-mac-2.local>
2017-07-09 17:27:12 +0100

    04- Debounce vs Throttle

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
 
     /* nav button click event */
+    var clicked = true;
 
-    $('header .nav-btn').click(function (e) { 
-        $(this).parent().toggleClass('active');
+    $('header .nav-btn').click(function (e) {
+
+    	var $this = $(this);
+    	if(clicked){
+
+    		clicked = false;
+        	$this.parent().toggleClass('active');
+        	setTimeout(function(){clicked = true}, 1450);
+
+
+    	}
+
+    	
         
     });
 
